76be6f5c7e2ba8e4bb943a7dc0b6d551963ca601
Jose Vicente <jvjfranca@gmail.com>
2023-02-03 09:57:17 -0300

    glue s3 dynamodb

--- glue-dynamodb.py
+++ glue-dynamodb.py
@@ -14,29 +14,34 @@ s3_bucket_1_path = "s3://your_s3_bucket_1_path"
 s3_bucket_2_path = "s3://your_s3_bucket_2_path"
 
 # Read data from first DynamoDB table
-dynamodb_table_1 = glueContext.create_dynamic_frame.from_options("dynamodb",
-                                                                 {
-                                                                     "table_name": dynamodb_table_1_name,
-                                                                     "region": "us-west-2"
-                                                                 })
+dynamodb_table_1 = glueContext.create_dynamic_frame.from_options(
+    "dynamodb",
+    {
+        "table_name": dynamodb_table_1_name,
+        "region": "us-west-2"
+    }
+)
 
 # Read data from second DynamoDB table
-dynamodb_table_2 = glueContext.create_dynamic_frame.from_options("dynamodb",
-                                                                 {
-                                                                     "table_name": dynamodb_table_2_name,
-                                                                     "region": "us-west-2"
-                                                                 })
+dynamodb_table_2 = glueContext.create_dynamic_frame.from_options(
+    "dynamodb",
+    {
+        "table_name": dynamodb_table_2_name,
+        "region": "us-west-2"
+    }
+)
 
 # Read data from S3 bucket
-s3_bucket_1 = glueContext.create_dynamic_frame.from_options("s3",
-                                                            {
-                                                                "paths": [s3_bucket_1_path]
-                                                            })
+s3_bucket_1 = glueContext.create_dynamic_frame.from_options(
+    "s3",
+    {
+        "paths": [s3_bucket_1_path]
+    }
+)
 
 # Join data from the S3 bucket, first DynamoDB table, and second DynamoDB table
-joined_data = Join.apply(s3_bucket_1, dynamodb_table_1, "s3_key", "dynamodb_1_key").join(dynamodb_table_2,
-                                                                                         "dynamodb_2_key",
-                                                                                         "dynamodb_2_key")
+joined_data = Join.apply(s3_bucket_1, dynamodb_table_1, "s3_key", "dynamodb_1_key").join(
+    dynamodb_table_2, "dynamodb_2_key", "dynamodb_2_key")
 
 # Use a user-defined function on the joined data
 
@@ -49,9 +54,11 @@ def process_data(data):
 processed_data = Map.apply(joined_data, process_data)
 
 # Write the processed data to the S3 bucket data sink
-glueContext.write_dynamic_frame.from_options(frame=processed_data,
-                                             connection_type="s3",
-                                             connection_options={
-                                                 "path": s3_bucket_2_path
-                                             },
-                                             format="parquet")
+glueContext.write_dynamic_frame.from_options(
+    frame=processed_data,
+    connection_type="s3",
+    connection_options={
+        "path": s3_bucket_2_path
+    },
+    format="parquet"
+)
